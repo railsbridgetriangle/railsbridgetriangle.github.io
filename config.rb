c1669cde55c0b87f0060abb26bf16231876411c9
Christian Reuter <christian@thoughtbot.com>
2014-12-04 15:58:32 -0500

    First pass at Resources page
    * nav_link helper for active page nav classes
    * Vendor Waypoints and StickyJs

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ activate :views
 activate :directory_indexes
 
 set :relative_links, true
-set :css_dir, 'assets/stylesheets'
-set :js_dir, 'assets/javascripts'
-set :images_dir, 'assets/images'
-set :fonts_dir, 'assets/fonts'
-set :layout, 'layouts/application'
+set :css_dir, "assets/stylesheets"
+set :js_dir, "assets/javascripts"
+set :images_dir, "assets/images"
+set :fonts_dir, "assets/fonts"
+set :layout, "layouts/application"
 
 configure :development do
  activate :livereload
@@ -22,4 +22,17 @@ end
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method = :git
+end
+
+helpers do
+  def nav_link(link_text, page_url, options = {})
+    options[:class] ||= ""
+    if current_page.url.length > 1
+      current_url = current_page.url.chop
+    else
+      current_url = current_page.url
+    end
+    options[:class] << " active" if page_url == current_url
+    link_to(link_text, page_url, options)
+  end
 end
--- source/assets/javascripts/application.js.coffee
+++ source/assets/javascripts/application.js.coffee
@@ -1,1 +1,13 @@
 //= require_tree .
+
+$ ->
+  $("[data-role='sticky']").sticky topSpacing: 50
+
+  $("[data-role='waypoint']").waypoint (->
+    name = $(this).attr("id")
+    navLinks = $("[data-role='waypoints'] a")
+    activeLink = $("[data-trigger='#{name}']")
+    navLinks.removeClass "active"
+    activeLink.addClass "active"
+  ),
+    offset: "35%"
